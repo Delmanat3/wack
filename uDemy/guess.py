289f155f665c8c52acdcf2106577bfcccaf23886
Nathan Delman <delman.nathan832@gmail.com>
2022-02-28 15:17:18 -0600

    we do little war crime

--- uDemy/guess.py
+++ uDemy/guess.py
@@ -1,11 +1,22 @@
-answer=5
-print("please enter a guees")
-guess=int(input())
+import random
+high=1000
+low=1
+guesses=1
+print("think of a number from {} to {}. I will guess it within 10 tries.".format(low,high))
 
-if guess<answer:
-    print("is wrong")
-
-if guess>answer:
-    print("is mucho wrong")
-elif guess== answer:
-    print("is good")
+answer=random.randint(low,high)
+while True:
+    guess = low + (high-low) // 2
+    x=input("my first guess is {} please press h if the number is higher l if the number is lower, or c if the guess is correct ".format(guess)).casefold()
+    if x=="h":
+        low=guess + 1
+       
+    elif x=="l":
+        high = guess-1
+        
+    elif x == "c":
+        print("noce i got it in {} tries".format(guesses))
+        break
+    else:
+        print("hit h,l,c")
+guesses+=guesses+1
